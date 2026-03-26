03076109000832e2a79ed814a064fc590b7dc887
Gourav Sahu <gouravsahu991@gmail.com>
2026-03-26 20:56:38 +0530

    Delete Frontend/src directory

--- Frontend/src/components/dashboard/ContributionNetworkWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import ContributionNetwork from "./ContributionNetwork";
-
-export default function ContributionNetworkWrapper() {
-  // Replaced dynamic import with standard component as it's now hydration-safe
-  return <ContributionNetwork />;
-}
